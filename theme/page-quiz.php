daad24346e97d8186953c3d1c67e251fba9fbe9c
Dan Wilt <dwilt4rville@gmail.com>
2018-03-19 15:09:50 +0100

    put in the quiz into the template

--- theme/page-quiz.php
+++ theme/page-quiz.php
@@ -4,7 +4,9 @@ Template Name: Quiz
 */
 get_header();
 ?>
-<main>
-
+<main id="root">
+    <script>console.log("Hey, don't cheat. Just do your best.");</script>
+    <link href="<?php bloginfo(‘template_directory’); ?>/static/css/main.8d2ec63d.css" rel="stylesheet">
+    <script type="text/javascript" src="<?php bloginfo(‘template_directory’); ?>/static/js/main.c4f6402c.js"></script>
 </main>
 <?php get_footer(); ?>
