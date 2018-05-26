e1708e405f5227462715882839032036dcc68dcd
Dan Wilt <dwilt4rville@gmail.com>
2018-05-26 01:34:46 -0400

    file fix

--- theme/page-quiz.php
+++ theme/page-quiz.php
@@ -7,7 +7,7 @@ get_header();
 <main>
     <div class="container" id="root"></div>
     <script>console.log("Hey, don't cheat. Just do your best.");</script>
-    <link href="/wp-content/themes/danwiltcoaching/static/css/main.002d7bff.css" rel="stylesheet">
-    <script type="text/javascript" src="/wp-content/themes/danwiltcoaching/static/js/main.382aaddf.js"></script>
+    <link href="/wp-content/themes/danwiltcoaching/static/css/main.623d1ece.css" rel="stylesheet">
+    <script type="text/javascript" src="/wp-content/themes/danwiltcoaching/static/js/main.55faa2d8.js"></script>
 </main>
 <?php get_footer(); ?>
