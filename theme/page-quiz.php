61784ce54812d36379ebcd480f8429198a234c7a
Dan Wilt <dwilt4rville@gmail.com>
2018-06-15 10:04:51 -0400

    updates to quiz

--- theme/page-quiz.php
+++ theme/page-quiz.php
@@ -7,7 +7,7 @@ get_header();
 <main>
     <div class="container" id="root"></div>
     <script>console.log("Hey, don't cheat. Just do your best.");</script>
-    <link href="/wp-content/themes/danwiltcoaching/static/css/main.623d1ece.css" rel="stylesheet">
-    <script type="text/javascript" src="/wp-content/themes/danwiltcoaching/static/js/main.55faa2d8.js"></script>
+    <link href="/wp-content/themes/danwiltcoaching/static/css/main.cb6b5455.css" rel="stylesheet">
+    <script type="text/javascript" src="/wp-content/themes/danwiltcoaching/static/js/main.b72ead82.js"></script>
 </main>
 <?php get_footer(); ?>
