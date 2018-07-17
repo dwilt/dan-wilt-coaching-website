bb425fa39499b35adebd3e5b7aac13710544718c
Jake Repash <jrep20@gmail.com>
2018-07-17 10:09:44 -0400

    viewport fit for iphonex

--- theme/header.php
+++ theme/header.php
@@ -7,7 +7,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, viewport-fit=cover">
         <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico">
         <!-- Facebook Pixel Code -->
         <script>
